5943b5274a58ca1f682a7ff160ad2379f78592c2
Mai Long <mailong245@gmail.com>
2021-05-28 19:51:10 +0700

    update code

---
+++ configclientaboutcompany/src/main/java/com/example/configclientaboutcompany/ConfigClientAboutCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.configclientaboutcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAboutCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAboutCompanyApplication.class, args);
+	}
+
+}
---
+++ configclientaboutcompany/src/test/java/com/example/configclientaboutcompany/ConfigClientAboutCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configclientaboutcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientAboutCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
